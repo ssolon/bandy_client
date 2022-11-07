5a3b535ae5c01a423c2533c73d912d63d8d5041a
ssolon <ssolon@usa.net>
2022-11-07 14:35:55 -0800

    Dummy exercise ids -> uuid
    They're going to eventually be UUID anyway.

--- lib/exercise/exercise_dummys.dart
+++ lib/exercise/exercise_dummys.dart
@@ -1,11 +1,11 @@
 /// For testing just define some dummy exercises
 
 Map<String, String> dummyExercises = {
-  'incline_press': 'Incline Chest Press',
-  'press': 'Chest Press',
-  'decline_press': 'Decline Chest Press',
-  'bar_curl': 'Bar curl',
-  'hammer_curl': 'Hammer Curl',
-  'pulldown': 'Triceps pulldown',
-  'goodmorning': 'Good Morning',
+  '78c4c452-1dfc-47a0-8b87-8fa6beead010': 'Incline Chest Press',
+  '24b5507f-e428-4db2-bab9-070561ea78df': 'Chest Press',
+  '781413e4-0df7-4f72-82f6-9b5697a90b34': 'Decline Chest Press',
+  'f8de849c-4df3-40f0-8114-42e17f26e5e7': 'Bar curl',
+  '0aa0f3eb-9121-4599-adef-52bf6eb619c8': 'Hammer Curl',
+  '71d9502a-8357-4609-ae63-7d8ca4f187c0': 'Triceps pulldown',
+  'd789d77a-c83e-41d5-a7e5-5e0f41bb340f': 'Good Morning',
 };
