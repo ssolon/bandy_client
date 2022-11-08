bc7d31aba75c94a19e746f5437768ca95c5ba725
ssolon <ssolon@usa.net>
2022-11-08 15:49:54 -0800

    Beginnings of (current) session handling
    Define a session.

---
+++ lib/workout_session/workout_session_notifier.dart
@@ -0,0 +1,20 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:uuid/uuid.dart';
+
+import 'workout_session_state.dart';
+
+part 'workout_session_notifier.g.dart';
+
+@riverpod
+class WorkoutSessionNotifier extends _$WorkoutSessionNotifier {
+  @override
+  WorkoutSessionState build() {
+    return WorkoutSessionState.initial();
+  }
+
+  /// Create a new session starting now
+  void start() {
+    state = WorkoutSessionState(
+        id: const Uuid().toString(), starting: DateTime.now(), sets: []);
+  }
+}
---
+++ lib/workout_session/workout_session_state.dart
@@ -0,0 +1,16 @@
+import 'package:bandy_client/routine/workout_set/logic/workout_set_state.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'workout_session_state.freezed.dart';
+
+@freezed
+class WorkoutSessionState with _$WorkoutSessionState {
+  factory WorkoutSessionState(
+      {required String id,
+      required DateTime starting,
+      required List<WorkoutSetState> sets}) = Data;
+
+  factory WorkoutSessionState.initial() = Initial;
+  factory WorkoutSessionState.loading() = Loading;
+  factory WorkoutSessionState.error([String? message]) = Error;
+}
