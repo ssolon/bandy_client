b34bec6a3f15f3e78f5e31cfcb740aed426012e4
ssolon <ssolon@usa.net>
2022-11-18 21:32:52 -0800

    Typo

--- lib/exercise/exercise_dummys.dart
+++ lib/exercise/exercise_dummys.dart
@@ -7,8 +7,8 @@ List<Exercise> dummyExerciseList = [
   Exercise(UuidValue('24b5507f-e428-4db2-bab9-070561ea78df'), 'Chest Press'),
   Exercise(
       UuidValue('78c4c452-1dfc-47a0-8b87-8fa6beead010'), 'Incline Chest Press'),
-  Exercise(UuidValue('781413e4-0df7-4f72-82f6-9b5697a90b34'),
-      'Decline Ches)t Press'),
+  Exercise(
+      UuidValue('781413e4-0df7-4f72-82f6-9b5697a90b34'), 'Decline Chest Press'),
   Exercise(UuidValue('f8de849c-4df3-40f0-8114-42e17f26e5e7'), 'Bar curl'),
   Exercise(UuidValue('0aa0f3eb-9121-4599-adef-52bf6eb619c8'), 'Hammer Curl'),
   Exercise(
