cc9cec297a39e0a7820aa25cc29492c4d807c783
ssolon <ssolon@usa.net>
2022-11-26 22:33:01 -0800

    Add "fly" exercise type

--- lib/exercise/exercise_dummys.dart
+++ lib/exercise/exercise_dummys.dart
@@ -9,6 +9,7 @@ List<Exercise> dummyExerciseList = [
       UuidValue('78c4c452-1dfc-47a0-8b87-8fa6beead010'), 'Incline Chest Press'),
   Exercise(
       UuidValue('781413e4-0df7-4f72-82f6-9b5697a90b34'), 'Decline Chest Press'),
+  Exercise(UuidValue('ac7a5720-5732-4a28-b200-10ce1cb072fd'), 'Fly'),
   Exercise(UuidValue('f8de849c-4df3-40f0-8114-42e17f26e5e7'), 'Bar curl'),
   Exercise(UuidValue('0aa0f3eb-9121-4599-adef-52bf6eb619c8'), 'Hammer Curl'),
   Exercise(
