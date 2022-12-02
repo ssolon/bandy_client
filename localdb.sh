03fdcf73bdb05005f6533e547fa3088f72206a6a
ssolon <ssolon@usa.net>
2022-12-02 15:48:58 -0800

    Utility to work with the sqlite database on the android device

---
+++ localdb.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+# Operations on sqlite db on android device (the "local" database on device)
+DB_PATH="/storage/emulated/0/Android/data/com.samuelsolon.bandy_client/files/bandy_client"
+DB_NAME="kaleidalog.db"
+
+case "$1" in
+    "pull")
+        adb pull $DB_PATH/$DB_NAME .
+        ;;
+
+    "ls")
+        adb shell ls $DB_PATH
+        ;;
+
+    "rm")
+        adb shell rm $DB_PATH/$DB_NAME
+        adb shell rm $DB_PATH/$DB_NAME-journal
+        ;;
+
+    *)
+        echo "Usage $0 pull | ls | rm"
+        ;;
+esac
