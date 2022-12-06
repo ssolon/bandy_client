b878067383fe7affb24708c107f45d573425ca00
ssolon <ssolon@usa.net>
2022-12-05 23:23:10 -0800

    Delete some boilerplate comments

--- lib/main.dart
+++ lib/main.dart
@@ -67,21 +67,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bandy',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Bandy Home Page'),
@@ -91,15 +81,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -119,36 +100,49 @@ class _MyHomePageState extends State<MyHomePage> {
     final device = defaultDevice;
 
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.devices),
-              tooltip: 'Scan',
-              onPressed: () => _gotoScanner(context),
-            ),
-          ],
-        ),
-        body: Column(
-          children: [
-            const WorkoutWidget(),
-            Expanded(
-              child: device == null
-                  ? Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const <Widget>[
-                          Text(
-                              'No Bandy device selected. Tap scan to find one'),
-                        ],
-                      ),
-                    )
-                  : DeviceDisplayWidget(defaultDevice!),
-            ),
-          ],
-        ));
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.devices),
+            tooltip: 'Scan',
+            onPressed: () => _gotoScanner(context),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const WorkoutWidget(),
+          Expanded(
+            child: device == null
+                ? Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Text('No Bandy device selected. Tap scan to find one'),
+                      ],
+                    ),
+                  )
+                : DeviceDisplayWidget(defaultDevice!),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {},
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Exercise',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.view_list),
+            label: 'Workout history',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
   }
 
   _gotoScanner(BuildContext context) async {
